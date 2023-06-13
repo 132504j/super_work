b9e173fa85e43679371092c11fea99c5d3277b48
jzyong <2298613382@qq.com>
2023-06-13 18:01:38 +0800

    a

--- service/main_server/src/main/java/cn/jzyong/main_server/MainServerApplication.java
+++ service/main_server/src/main/java/cn/jzyong/main_server/MainServerApplication.java
@@ -2,8 +2,10 @@ package cn.jzyong.main_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("cn.jzyong.*")
 public class MainServerApplication {
 
     public static void main(String[] args) {
